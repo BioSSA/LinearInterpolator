13d2dd40ceaaf4de24c6969b7c56a998aa0d96ce
Svyatoslav Gryaznov <nightuser@ya.ru>
2014-12-23 13:25:29 +0300

    Make it compile

--- R/linear.interpolate.R
+++ R/linear.interpolate.R
@@ -1,6 +1,6 @@
 linear.interpolate <- function(x, points, values, fill_value=NA, scale=FALSE) {
   #stopifnot(load.python())
- 
+
   x <- as.matrix(x)
   points <- as.matrix(points)
   values <- as.vector(values)
@@ -36,7 +36,7 @@ linear.interpolate <- function(x, points, values, fill_value=NA, scale=FALSE) {
   storage.mode(x) <- storage.mode(points) <- storage.mode(values) <- storage.mode(fill_value) <- storage.mode(scale) <- "double"
   storage.mode(d) <- "integer"
 
-  res <- .Call(linear_interpolate_d, d, points, values, x, fill_value, scale)
+  res <- .Call(linear_interpolate_d, d, points, values, x, fill_value)
 
   res <- lapply(res, function(x) if (is.null(x)) NA_real_ else as.double(x))
   res <- unlist(res)
