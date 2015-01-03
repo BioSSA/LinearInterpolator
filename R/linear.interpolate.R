bbc47ca8df0a665a8fa7e296ae9747869e90599c
Alexander Shlemov <shlemovalex@gmail.com>
2015-01-04 02:15:39 +0300

    Remove extra convertions
    It was some copy-paste from original Alex's code

--- R/linear.interpolate.R
+++ R/linear.interpolate.R
@@ -97,15 +97,5 @@ linear.interpolate <- function(x, points, values, fill_value=NA, scale=FALSE, ci
   storage.mode(x) <- storage.mode(points) <- storage.mode(values) <- storage.mode(fill_value) <- storage.mode(scale) <- "double"
   storage.mode(d) <- "integer"
 
-
-
-  res <- .Call(linear_interpolate_d, d, points, values, x, fill_value, scale)
-
-  res <- lapply(res, function(x) if (is.null(x)) NA_real_ else as.double(x))
-  res <- unlist(res)
-  res <- as.double(res)
-
-  res[is.nan(res)] <- NA_real_
-
-  res
+  .Call("linear_interpolate_d", d, points, values, x, fill_value, scale)
 }
